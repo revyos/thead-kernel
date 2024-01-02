0b4bff2478583738124ca54dab5abed9db983300
huaming.jiang <jianghuaming@linux.alibaba.com>
2024-01-02 19:45:37 +0800

    lpi4a add wakeup gpio key and usb resume fix
    Change-Id: I0479ed6cd676af7ad087968e1bb22f2b04e89337

--- drivers/usb/dwc3/dwc3-thead.c
+++ drivers/usb/dwc3/dwc3-thead.c
@@ -257,10 +257,23 @@ static int dwc3_thead_remove(struct platform_device *pdev)
 static int dwc3_thead_pm_suspend(struct device *dev)
 {
 	struct dwc3_thead *thead = dev_get_drvdata(dev);
-
+    int ret;
 	dwc3_thead_assert(thead);
 
 	clk_bulk_disable(thead->num_clocks, thead->clks);
+    ret = regulator_disable(thead->vbus);
+    if (ret) {
+			dev_err(dev, "failed to disable regulator vbus %d\n", ret);
+    }
+    ret = regulator_disable(thead->hub1v2);
+    if (ret) {
+        dev_err(dev, "failed to disable regulator hub1v2 %d\n", ret);
+    }
+    ret = regulator_disable(thead->hub5v);
+
+    if (ret) {
+        dev_err(dev, "failed to disable regulator hub1v2 %d\n", ret);
+    }
 
 	return 0;
 }
@@ -270,6 +283,20 @@ static int dwc3_thead_pm_resume(struct device *dev)
 {
 	struct dwc3_thead *thead = dev_get_drvdata(dev);
 	int ret;
+    dev_info(dev,"%s\n",__func__);
+    ret = regulator_enable(thead->vbus);
+    if (ret) {
+			dev_err(dev, "failed to enable regulator vbus %d\n", ret);
+    }
+    ret = regulator_enable(thead->hub1v2);
+    if (ret) {
+        dev_err(dev, "failed to enable regulator hub1v2 %d\n", ret);
+    }
+    ret = regulator_enable(thead->hub5v);
+
+    if (ret) {
+        dev_err(dev, "failed to enable regulator hub1v2 %d\n", ret);
+    }
 
 	ret = clk_bulk_prepare_enable(thead->num_clocks, thead->clks);
 	if (ret) {
